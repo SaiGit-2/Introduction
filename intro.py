7fe86d55913bb02d88619f2cacf9856eff83ffd6
Sai Subbu <spolavar87@gmail.com>
2024-12-25 00:41:18 -0500

    Changed code

--- intro.py
+++ intro.py
@@ -1,9 +1,9 @@
-"""This module prints python version, path location and tries to perform get on coreyms website."""
+"""This module prints python version, path location and tries to perform get on yahoo website."""
 
 import requests
 
 
 print("Hello World!")
-r = requests.get("https://coreyms.com", timeout=10)
+r = requests.get("https://yahoo.com", timeout=10)
 print(r.status_code)
 print(r.ok)
