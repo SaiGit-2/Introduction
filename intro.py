588da79d15f89b9409f87328ac181395e6104a75
Sai Subbu <spolavar87@gmail.com>
2024-12-24 23:34:20 -0500

    Final Code

--- intro.py
+++ intro.py
@@ -1,11 +1,9 @@
 """This module prints python version, path location and tries to perform get on coreyms website."""
 
-import sys
 import requests
 
 
-print(sys.version)
-print(sys.executable)
 print("Hello World!")
 r = requests.get("https://coreyms.com", timeout=10)
 print(r.status_code)
+print(r.ok)
